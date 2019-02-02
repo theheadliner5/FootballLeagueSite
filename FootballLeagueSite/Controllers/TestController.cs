f5f56535118377e2ac8d682ee4d7d9fc838fc68c
Karol Chłopik <theheadliner995@gmail.com>
2019-02-02 04:53:20 +0100

    re

---
+++ FootballLeagueSite/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Web;
+using Umbraco.Core;
+using System.Web.Http;
+using Umbraco.Web.WebApi;
+
+namespace FootballLeagueSite.Controllers
+{
+    public class TestController : UmbracoApiController
+    {
+        [HttpGet]
+        public Boolean testAction()
+        {
+            var contentService = ApplicationContext.Current.Services.ContentService;
+            var publishDate = DateTime.Now;
+            var content = contentService.CreateContent("Leeds-United", 1164, "Team");
+            content.ReleaseDate = publishDate;
+            content.CreateDate = publishDate;
+            content.UpdateDate = publishDate;
+            content.SetValue("pageTitle", "lol2");
+            var result = contentService.SaveAndPublishWithStatus(content);
+
+            return result.Success;
+        }
+    }
+}
